15ef894e699fd6369e9bc14309dd21474a318ff3
ngocphm <minhngoc1122000@gmail.com>
2022-07-23 16:19:35 +0700

    23/07 lan2

--- app/src/main/java/com/app/projectfinal/fragment/UserFragment.java
+++ app/src/main/java/com/app/projectfinal/fragment/UserFragment.java
@@ -1,16 +1,21 @@
 package com.app.projectfinal.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.app.projectfinal.R;
+import com.app.projectfinal.activity.myshop;
+import com.app.projectfinal.activity.shop_settings;
 
 public class UserFragment extends Fragment {
-
+    private TextView textView ;
 
     public UserFragment() {
         // Required empty public constructor
@@ -21,7 +26,13 @@ public class UserFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-
+        textView = view.findViewById(R.id.linear_user_ban);
+//        textView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getActivity(),shop_settings.class));
+//            }
+//        });
         return view;
     }
 
