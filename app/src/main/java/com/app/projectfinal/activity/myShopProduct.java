be3228b177508f0ea5eec50dd0f69c0ee38a4d6f
ngocphm <minhngoc1122000@gmail.com>
2022-08-01 00:33:04 +0700

    01/08

---
+++ app/src/main/java/com/app/projectfinal/activity/myShopProduct.java
@@ -0,0 +1,14 @@
+package com.app.projectfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myShopProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_shop_product);
+    }
+}
